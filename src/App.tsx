b8b2501e3f11de26fd3cf41e055789323177e93c
Rudolf Hersén <rudolf.hersen@schibsted.com>
2022-09-05 16:27:14 +0200

    SSE callback doesn't work with hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,85 @@
-import { useEffect, useState } from "react";
+import { Component } from "react";
 import "./App.css";
 import { add, formatISO, sub } from "date-fns";
+import TrainAnnouncement from "./TrainAnnouncement";
 import Table from "./Table";
 import { SearchParams } from "./SearchParams";
-import TrainAnnouncement from "./TrainAnnouncement";
 
 let intervalId: NodeJS.Timeout;
 
-function queryString(params: SearchParams) {
-  return params.location
-    ? `?location=${params.location}`
-    : `?train=${params.trainId}`;
-}
+type MyState = {
+  locations: { [key: string]: string };
+  announcements: TrainAnnouncement[];
+  msg: string;
+  now: Date;
+  eventSource?: EventSource;
+};
 
-function App() {
-  const [locations, setLocations] = useState<Record<string, string>>({});
-  const [announcements, setAnnouncements] = useState<TrainAnnouncement[]>([]);
-  const [msg, setMsg] = useState("");
-  const [now, setNow] = useState(new Date());
-  const [eventSource, setEventSource] = useState<EventSource>();
+class App extends Component {
+  state: MyState = {
+    locations: {},
+    announcements: [],
+    msg: "",
+    now: new Date(),
+  };
 
-  function button(location: string) {
-    return (
-      <button
-        onClick={async () => {
-          const response = await fetch(
-            `/.netlify/functions/announcements?location=${location}`
-          );
-          const json = await response.json();
-          if (json.msg) setMsg(json.msg);
-          if (json.TrainAnnouncement) {
-            setAnnouncements(json.TrainAnnouncement);
-            setMsg("");
-          }
-        }}
-      >
-        {locations[location] ?? location}
-      </button>
-    );
+  async componentDidMount() {
+    intervalId = setInterval(() => this.setState({ now: new Date() }), 990);
+    const response = await fetch("/.netlify/functions/locations");
+    this.setState({ locations: await response.json() });
   }
 
-  useEffect(() => {
-    async function componentDidMount() {
-      intervalId = setInterval(() => setNow(new Date()), 990);
-      const response = await fetch("/.netlify/functions/locations");
-      setLocations(await response.json());
+  componentWillUnmount() {
+    clearInterval(intervalId);
+  }
+
+  render() {
+    function queryString(params: SearchParams) {
+      return params.location
+        ? `?location=${params.location}`
+        : `?train=${params.trainId}`;
     }
 
-    function componentWillUnmount() {
-      clearInterval(intervalId);
-    }
+    const since = formatISO(sub(new Date(), { hours: 2 })).substring(0, 19);
+    const until = formatISO(add(new Date(), { hours: 2 })).substring(0, 19);
+    const { msg, announcements, now, locations } = this.state;
 
-    componentDidMount();
+    return (
+      <div>
+        {this.button("Sub")}
+        {this.button("Sod")}
+        {this.button("Sci")}
+        {this.button("Sst")}
+        {this.button("Åbe")}
+        {this.button("Äs")}
+        {this.button("Sta")}
+        {this.button("Hu")}
+        {this.button("Flb")}
+        {this.button("Tul")}
+        {this.button("Tu")}
+        <div>{msg}</div>
+        <Table
+          locations={locations}
+          announcements={announcements}
+          now={now}
+          fetch={async (params: SearchParams) => {
+            const rsp = await fetch(
+              `/.netlify/functions/announcements${queryString(
+                params
+              )}&since=${since}&until=${until}`
+            );
+            const json = await rsp.json();
 
-    return componentWillUnmount;
-  }, []);
-
-  const since = formatISO(sub(new Date(), { hours: 2 })).substring(0, 19);
-  const until = formatISO(add(new Date(), { hours: 2 })).substring(0, 19);
-
-  return (
-    <div>
-      {button("Sub")}
-      {button("Sod")}
-      {button("Sci")}
-      {button("Sst")}
-      {button("Åbe")}
-      {button("Äs")}
-      {button("Sta")}
-      {button("Hu")}
-      {button("Flb")}
-      {button("Tul")}
-      {button("Tu")}
-      <div>{msg}</div>
-      <Table
-        locations={locations}
-        announcements={announcements}
-        now={now}
-        fetch={async (params: SearchParams) => {
-          const rsp = await fetch(
-            `/.netlify/functions/announcements${queryString(
-              params
-            )}&since=${since}&until=${until}`
-          );
-          const json = await rsp.json();
-
-          if (json.INFO) {
-            eventSource?.close();
-            setEventSource(new EventSource(json.INFO.SSEURL));
-
-            if (eventSource) {
+            if (json.INFO) {
+              this.state.eventSource?.close();
+              const eventSource = new EventSource(json.INFO.SSEURL);
+              const self = this;
               eventSource.onmessage = ({ data }: MessageEvent) => {
                 const trainAnnouncement: TrainAnnouncement[] =
                   JSON.parse(data).RESPONSE.RESULT[0].TrainAnnouncement;
-                {
+                self.setState(({ announcements }: MyState) => {
                   const found = announcements.findIndex(
-                    (announcement: TrainAnnouncement) =>
+                    (announcement) =>
                       announcement.LocationSignature ===
                       trainAnnouncement[0].LocationSignature
                   );
@@ -108,17 +92,42 @@ function App() {
                         announcements.slice(found + 1)
                       ),
                   };
-                }
+                });
               };
+              this.setState({ eventSource });
             }
-          }
-          if (json.msg) setMsg(json.msg);
-          setAnnouncements(json.TrainAnnouncement);
-          setMsg("");
+            if (json.msg) this.setState({ msg: json.msg });
+            this.setAnnouncements(json.TrainAnnouncement);
+          }}
+        />
+      </div>
+    );
+  }
+
+  private setAnnouncements(announcements: Array<TrainAnnouncement>) {
+    if (announcements) this.setState({ announcements, msg: "" });
+  }
+
+  button(location: string) {
+    return (
+      <button
+        onClick={async () => {
+          const response = await fetch(
+            `/.netlify/functions/announcements?location=${location}`
+          );
+          const json = await response.json();
+          if (json.msg) this.setState({ msg: json.msg });
+          if (json.TrainAnnouncement)
+            this.setState({
+              announcements: json.TrainAnnouncement,
+              msg: "",
+            });
         }}
-      />{" "}
-    </div>
-  );
+      >
+        {this.state.locations[location]}
+      </button>
+    );
+  }
 }
 
 export default App;
